1f7d2579c942ca71df5abe46fd7dfe466b8e60af
Kriz Edward <edwardthemangare@gmail.com>
2020-05-25 21:55:38 +0700

    (v.1.7)_create_seeder_borrow

---
+++ database/seeds/BorrowTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Borrow;
+
+class BorrowTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Borrow::create([
+        	'id'		=> '1',
+			'book_id'	=> '1',
+			'user_id'	=> '1',
+			'status'	=> 'Menunggu Verifikasi',
+        ]);
+    }
+}
